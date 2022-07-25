eb2bae76e420205f3205679c75c6322025c1eb28
Jose Carlos <jo_carlos07@hotmail.com>
2022-07-25 11:59:29 -0500

    7ma Actualización

--- MainScreen.aspx.cs
+++ MainScreen.aspx.cs
@@ -37,67 +37,67 @@ namespace MVP_ASP
             string connection = "Server=localhost;UserID=root;Database=Sedlaxar;Password=root;";
 
             using (MySqlConnection connectINV = new MySqlConnection(connection))
+            {
+                connectINV.Open();
+                using (MySqlCommand cmd = new MySqlCommand("CREATE TABLE contras (con_admin TEXT, con_mast TEXT)", connectINV))
                 {
-                    connectINV.Open();
-                    using (MySqlCommand cmd = new MySqlCommand("CREATE TABLE contras (con_admin TEXT, con_mast TEXT)", connectINV))
-                    {
-                        cmd.BeginExecuteNonQuery();
-                    }
-                    using (MySqlCommand cmd = new MySqlCommand("CREATE TABLE direccion (dirLocal TEXT, correo TEXT)", connectINV))
-                    {
-                        cmd.BeginExecuteNonQuery();
-                    }
-                    using (MySqlCommand cmd = new MySqlCommand("CREATE TABLE audit (fecha TEXT, hora TEXT, operacion TEXT, cambioRealizado TEXT)", connectINV))
-                    {
-                        cmd.BeginExecuteNonQuery();
-                    }
-                    using (MySqlCommand cmd = new MySqlCommand("CREATE TABLE provedores (clave TEXT, provedor TEXT, correo TEXT, telefono TEXT)", connectINV))
-                    {
-                        cmd.BeginExecuteNonQuery();
-                    }
-                    using (MySqlCommand cmd = new MySqlCommand("CREATE TABLE folios (ID INTEGER, noFolio TEXT)", connectINV))
-                    {
-                        cmd.BeginExecuteNonQuery();
-                    }
-                    using (MySqlCommand cmd = new MySqlCommand("CREATE TABLE inventario (clave TEXT, producto TEXT, presentacion TEXT, precio REAL, costo REAL, montounit REAL, cantidad INTEGER, limite_inferior INTEGER, iva REAL)", connectINV))
-                    {
-                        cmd.BeginExecuteNonQuery();
-                    }
-                    using (MySqlCommand cmd = new MySqlCommand("CREATE TABLE ventas (vendedor TEXT, folioAsociado TEXT)", connectINV))
-                    {
-                        cmd.BeginExecuteNonQuery();
-                    }
-                    using (MySqlCommand cmd = new MySqlCommand("CREATE TABLE vendedores (claveVendedor TEXT, nombre TEXT, ventas INTEGER, puesto TEXT, salario TEXT)", connectINV))
-                    {
-                        cmd.BeginExecuteNonQuery();
-                    }
-
-                    using (MySqlCommand cmd = new MySqlCommand("CREATE TABLE proveedores (clave TEXT, proveedor TEXT, tel INTEGER, correo TEXT)", connectINV))
-                    {
-                        cmd.BeginExecuteNonQuery();
-                    }
-
-                    using (MySqlCommand cmd = new MySqlCommand("INSERT INTO contras (con_admin, con_mast) VALUES (@conAdmin, @conMaestra)", connectINV))
-                    {
-                        cmd.Parameters.AddWithValue("@conAdmin", (MySqlDbType)DbType.AnsiString).Value = "0";
-                        byte[] tmpSource;
-                        byte[] tmpHash;
-                        tmpSource = ASCIIEncoding.ASCII.GetBytes("S3DL4X4R?¿DSNPr0?");
-                        tmpHash = new MD5CryptoServiceProvider().ComputeHash(tmpSource);
-                        cmd.Parameters.AddWithValue("@conMaestra", (MySqlDbType)DbType.AnsiString).Value = BitConverter.ToString(tmpHash).Replace("-", "");
-                        cmd.BeginExecuteNonQuery();
-                    }
-                    using (MySqlCommand cmd = new MySqlCommand("INSERT INTO folios (ID, noFolio) VALUES (@id, @folio)", connectINV))
-                    {
-                        cmd.Parameters.AddWithValue("@id", (MySqlDbType)DbType.Int32).Value = 1;
-                        cmd.Parameters.AddWithValue("@folio", (MySqlDbType)DbType.AnsiString).Value = "A000000";
-                        cmd.BeginExecuteNonQuery();
-                    }
+                    cmd.BeginExecuteNonQuery();
                 }
-                MySqlConnection.ClearAllPools();
+                using (MySqlCommand cmd = new MySqlCommand("CREATE TABLE direccion (dirLocal TEXT, correo TEXT)", connectINV))
+                {
+                    cmd.BeginExecuteNonQuery();
+                }
+                using (MySqlCommand cmd = new MySqlCommand("CREATE TABLE audit (fecha TEXT, hora TEXT, operacion TEXT, cambioRealizado TEXT)", connectINV))
+                {
+                    cmd.BeginExecuteNonQuery();
+                }
+                using (MySqlCommand cmd = new MySqlCommand("CREATE TABLE provedores (clave TEXT, provedor TEXT, correo TEXT, telefono TEXT)", connectINV))
+                {
+                    cmd.BeginExecuteNonQuery();
+                }
+                using (MySqlCommand cmd = new MySqlCommand("CREATE TABLE folios (ID INTEGER, noFolio TEXT)", connectINV))
+                {
+                    cmd.BeginExecuteNonQuery();
+                }
+                using (MySqlCommand cmd = new MySqlCommand("CREATE TABLE inventario (clave TEXT, producto TEXT, presentacion TEXT, precio REAL, costo REAL, montounit REAL, cantidad INTEGER, limite_inferior INTEGER, iva REAL)", connectINV))
+                {
+                    cmd.BeginExecuteNonQuery();
+                }
+                using (MySqlCommand cmd = new MySqlCommand("CREATE TABLE ventas (vendedor TEXT, folioAsociado TEXT)", connectINV))
+                {
+                    cmd.BeginExecuteNonQuery();
+                }
+                using (MySqlCommand cmd = new MySqlCommand("CREATE TABLE vendedores (claveVendedor TEXT, nombre TEXT, ventas INTEGER, puesto TEXT, salario TEXT)", connectINV))
+                {
+                    cmd.BeginExecuteNonQuery();
+                }
+
+                using (MySqlCommand cmd = new MySqlCommand("CREATE TABLE proveedores (clave TEXT, proveedor TEXT, tel INTEGER, correo TEXT)", connectINV))
+                {
+                    cmd.BeginExecuteNonQuery();
+                }
+
+                using (MySqlCommand cmd = new MySqlCommand("INSERT INTO contras (con_admin, con_mast) VALUES (@conAdmin, @conMaestra)", connectINV))
+                {
+                    cmd.Parameters.AddWithValue("@conAdmin", (MySqlDbType)DbType.AnsiString).Value = "0";
+                    byte[] tmpSource;
+                    byte[] tmpHash;
+                    tmpSource = ASCIIEncoding.ASCII.GetBytes("S3DL4X4R?¿DSNPr0?");
+                    tmpHash = new MD5CryptoServiceProvider().ComputeHash(tmpSource);
+                    cmd.Parameters.AddWithValue("@conMaestra", (MySqlDbType)DbType.AnsiString).Value = BitConverter.ToString(tmpHash).Replace("-", "");
+                    cmd.BeginExecuteNonQuery();
+                }
+                using (MySqlCommand cmd = new MySqlCommand("INSERT INTO folios (ID, noFolio) VALUES (@id, @folio)", connectINV))
+                {
+                    cmd.Parameters.AddWithValue("@id", (MySqlDbType)DbType.Int32).Value = 1;
+                    cmd.Parameters.AddWithValue("@folio", (MySqlDbType)DbType.AnsiString).Value = "A000000";
+                    cmd.BeginExecuteNonQuery();
+                }
+            }
      
             adm_button.Enabled = true;
             usr_button.Enabled = true;
+            MySqlConnection.ClearAllPools();
 
         }
 
